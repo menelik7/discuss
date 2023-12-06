44b50f34a479434dfb4ef263b7e5c7aa5f6485dd
menelik7 <menelikworku@gmail.com>
2023-12-06 09:40:43 +0100

    Setup paths with server actions and TODO on the server actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,6 @@ import { auth } from "@/auth";
 import Profile from "@/components/profile";
 
 export default async function Home() {
-	const session = await auth();
-
 	return (
 		<div>
 			<div className="flex items-center gap-2">
