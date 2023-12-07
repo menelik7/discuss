a02b98b55bf7286df5a217a6bc124e3ac76cc389
menelik7 <menelikworku@gmail.com>
2023-12-07 13:50:51 +0100

    Test filename

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@nextui-org/react";
 import * as actions from "@/actions";
-import { auth } from "@/auth";
 import Profile from "@/components/Profile";
 
 export default async function Home() {
