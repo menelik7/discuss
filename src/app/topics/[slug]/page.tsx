9556aa317b0da5f895cbf8f2f5f5133a153d1430
menelik7 <menelikworku@gmail.com>
2023-12-10 12:22:48 +0100

    Creating and displaying comments

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import PostCreateForm from "@/components/posts/PostCreateForm";
+import PostList from "@/components/posts/PostList";
+import { fetchPostsByTopicsSlug } from "@/db/queries/posts";
 
 interface TopicShowPageProps {
 	params: {
@@ -13,6 +15,7 @@ export default function TopicShowPage({ params }: TopicShowPageProps) {
 		<div className="grid grid-cols-4 gap-4 p-4">
 			<div className="col-span-3">
 				<h1 className="text-2xl font-bold mb-2">{slug}</h1>
+				<PostList fetchData={() => fetchPostsByTopicsSlug(slug)} />
 			</div>
 
 			<div>
